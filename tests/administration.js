350764760ae4b5d3c394fe0323b286366403b758
Sean Dawson <sean@two-bulls.com>
2013-10-13 18:05:24 +1100

    Added some more tests

--- tests/administration.js
+++ tests/administration.js
@@ -65,6 +65,36 @@ describe('Administration API tests', function() {
                   });
     });
 
+    it('Attempt to create existing user [0]', function(done) {
+        agent.put('http://localhost:3000/users/' +  users[0].username + '/')
+            .send(users[0])
+                  .end(function(e, res) {
+                    expect(e).to.eql(null);
+                    expect(res.status).to.be(409);
+                    done();
+                  });
+    });
+
+  it('Create user [1]', function(done) {
+        agent.put('http://localhost:3000/users/' +  users[1].username + '/')
+            .send(users[1])
+                  .end(function(e, res) {
+                    expect(e).to.eql(null);
+                    expect(res.status).to.be(201);
+                    done();
+                  });
+    });
+
+   it('Create user [2]', function(done) {
+        agent.put('http://localhost:3000/users/' +  users[2].username + '/')
+            .send(users[2])
+                  .end(function(e, res) {
+                    expect(e).to.eql(null);
+                    expect(res.status).to.be(201);
+                    done();
+                  });
+    });
+
 
     it('Login user [0] with incorrect password', function(done) {
         var u = 
@@ -140,9 +170,7 @@ describe('Administration API tests', function() {
                 expect(res.status).to.be(401);
                 done();
             });
-     
-
-        });
+    });
 
     it('Attempt to change password for user [0] with incorrect password', function(done) {
         agent.post('http://localhost:3000/users/' + users[0].username + '/password/')
@@ -197,33 +225,61 @@ describe('Administration API tests', function() {
             });
     });
 
-   it('Logout user [0]', function(done) {
-        agent.get('http://localhost:3000/logout/')
-            .send()
-                  .end(function(e, res) {
-                    expect(e).to.eql(null);
-                    expect(res.ok).to.be.ok();
-                    done();
-                  });
+    
+    it('Delete user [0]', function(done) {
+        agent.del('http://localhost:3000/users/' + users[0].username + '/')
+         .end(function(e, res) {
+            expect(e).to.eql(null);
+            expect(res.status).to.be(200);
+            done();
+        });
+    });
 
-    }); 
+    it('Attempt to login user [0] with new password after deletion', function(done) {
 
-    it('Change password for user [0] back to default', function(done) {
-        agent.post('http://localhost:3000/users/' + users[0].username + '/password/')
+        agent.post('http://localhost:3000/login/')
             .send({
-                'oldPassword' : 'new password',
-                'newPassword' : users[0].password
+                username : users[0].username,
+                password : 'new password'
 
             })
             .end(function(e, res) {
                 expect(e).to.eql(null);
-                expect(res.ok).to.be.ok();
+                expect(res.status).to.be(401);
                 done();
-            }); 
-    });   
-
-
+            });
+    });
 
  
+    it('Login user [1]', function(done) {
+        agent.post('http://localhost:3000/login/')
+            .send(users[1])
+                  .end(function(e, res) {
+                    expect(e).to.eql(null);
+                    expect(res.status).to.be(200);
+                    done();
+                  });
+
+    });
+
+     it('Attempt to delete user [2] logged in as user [1]', function(done) {
+        agent.del('http://localhost:3000/users/' + users[2].username + '/')
+             .end(function(e, res) {
+                expect(e).to.eql(null);
+                expect(res.status).to.be(401);
+                done();
+            });
+    });
+
+    it('Logout user [1]', function(done) {
+        agent.get('http://localhost:3000/logout/')
+            .send()
+            .end(function(e, res) {
+                expect(e).to.eql(null);
+                expect(res.ok).to.be.ok();
+                done();
+            });
+    }); 
+
 
 });
