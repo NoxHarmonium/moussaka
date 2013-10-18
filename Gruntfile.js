a4f157522a2b86955090a7e93f77c8105265d381
Sean Dawson <sean@two-bulls.com>
2013-10-19 10:05:34 +1100

    Made jshint much more strict and conform to a style. Added a beautifier to help keep the code strict automatically

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,63 @@
 module.exports = function(grunt) {
+   var srcFiles = [
+      'Gruntfile.js', 
+      'app.js', 
+      'api_modules/*.js',
+       'tests/*.js', 
+       'schemas/*.js',
+        'routes/*.js'
+   ];
 
    grunt.initConfig({
       pkg: grunt.file.readJSON('package.json'),
+      jsbeautifier: {
+         files: srcFiles,
+         options: 
+         {
+            js: {
+              braceStyle: "collapse",
+              breakChainedMethods: true,
+              e4x: true,
+              evalCode: false,
+              indentChar: " ",
+              indentLevel: 0,
+              indentSize: 2,
+              indentWithTabs: false,
+              jslintHappy: true,
+              keepArrayIndentation: false,
+              keepFunctionIndentation: false,
+              maxPreserveNewlines: 10,
+              preserveNewlines: true,
+              spaceBeforeConditional: true,
+              spaceInParen: false,
+              unescapeStrings: false,
+              wrapLineLength: 80,
+              goodStuff: true
+            }
+         }
+      },
       jshint: {
-         files: ['Gruntfile.js', 'app.js', 'api_modules/*.js', 'tests/*.js', 'schemas/*.js', 'routes/*.js'],
+         files: srcFiles,
          options: {
             globals: {
                console: true,
                modules: true,
-               document: true
+               document: true,
+               bitwise: true,
+               curly: true,
+               eqeqeq: true,
+               immed: true,
+               indent: 2,
+               latedef: true,
+               newcap : true,
+               noarg: true,
+               nonew: true,
+               quotmark: "single",
+               undef: true,
+               unused: true,
+               strict: true,
+               trailing: true,
+               maxlen: 80
             }
          }
       }
@@ -16,8 +65,9 @@ module.exports = function(grunt) {
 
 
    grunt.loadNpmTasks('grunt-contrib-jshint');
+   grunt.loadNpmTasks('grunt-jsbeautifier');
   
-   grunt.registerTask('default', ['jshint']);
+   grunt.registerTask('default', ['jsbeautifier', 'jshint']);
 
 
 };
