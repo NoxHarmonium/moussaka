643eda280bad467c565998b273f32f16ceaf6ce7
Sean Dawson <sean@two-bulls.com>
2013-10-19 09:18:10 +1100

    Added a grunt file that runs jshint

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+
+   grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
+      jshint: {
+         files: ['Gruntfile.js', 'app.js', 'api_modules/*.js', 'tests/*.js', 'schemas/*.js', 'routes/*.js'],
+         options: {
+            globals: {
+               console: true,
+               modules: true,
+               document: true
+            }
+         }
+      }
+   });
+
+
+   grunt.loadNpmTasks('grunt-contrib-jshint');
+  
+   grunt.registerTask('default', ['jshint']);
+
+
+}
