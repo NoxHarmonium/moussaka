1ee546db215a22ad004eb96843f4bb0918717498
Sean Dawson <sean@two-bulls.com>
2013-10-18 21:37:13 +1100

    Committed missing javascript file needed for commit 280f5ce

---
+++ tests/testdata.js
@@ -0,0 +1,18 @@
+/*
+    Shared data used for API testing
+*/
+
+exports.testUsers = [
+        { 
+            username: 'test.account@test.com',
+            password: 'test_password'
+        },
+        { 
+            username: 'test.account2@test.com',
+            password: 'test_password3'
+        },
+        { 
+            username: 'test.account3@test.com',
+            password: 'test_password4'
+        },
+    ];
