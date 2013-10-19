d58ca65c2821ae56f3d694674c6656d48e15297f
Sean Dawson <sean@two-bulls.com>
2013-10-19 14:24:54 +1100

    Added initial project schema

--- schemas/project.js
+++ schemas/project.js
@@ -3,13 +3,39 @@
 
   var mongoose = require('mongoose');
   var config = require('../include/config');
+  var Schema = mongoose.Schema;
 
+  var ProjectSchema = new Schema({
+    name: {
+      type: String,
+      required: true,
+      index: {
+        unique: true
+      }
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    users: [{
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }],
+    snapshots: [{
+      timestamp: {
+        type: Date,
+        default: Date.now
+      },
+      publisher: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      },
+      overlay: {
+        type: Schema.Types.Mixed
+      }
+    }]
+  });
 
-     
-
-     
-  };
-
-
+  module.exports = mongoose.model('Project', ProjectSchema);
 
 })(require, module);
