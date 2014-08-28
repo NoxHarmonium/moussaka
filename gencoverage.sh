b8658683cc8fa2d49fce446f394e4e9fed71b750
Sean Dawson <sean@two-bulls.com>
2014-08-28 19:33:06 -0300

    Added a script to upload code coverage to coveralls.io.

---
+++ gencoverage.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [[ -z "$COVERALLS_REPO_TOKEN" ]];
+then
+    export COVERALLS_SERVICE_NAME="Adhoc Test Machine";
+    export COVERALLS_REPO_TOKEN="1DiClwfewqa0eVver6pMUQY8JnALfgqka";
+    echo "Not on Travis CI? The the correct ENV vars have been set for you.";
+else 
+    echo "Coveralls info is already set. This should be so on Travis CI."
+fi
+
+FILES="tests/utils.test.js tests/controlValidation.test.js tests/userApi.test.js tests/projectApi.test.js tests/deviceApi.test.js tests/profileApi.test.js"
+istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- -R spec -b $FILES && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rm -rf ./coverage
