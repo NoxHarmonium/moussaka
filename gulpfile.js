0a44b3c4ec9a2e464728c2b76719b82d43f594d8
Sean Dawson <sean@two-bulls.com>
2014-09-15 18:20:41 -0300

    Added browserify and less to gulpfile.js.

--- gulpfile.js
+++ gulpfile.js
@@ -6,20 +6,57 @@ var gulp = require('gulp'),
   mocha = require('gulp-mocha'),
   istanbul = require('gulp-istanbul'),
   changed = require('gulp-changed'),
-  cache = require('gulp-cached');
+  cache = require('gulp-cached'),
+  browserify = require('gulp-browserify'),
+  less = require('gulp-less');
+
+var isWatching = false;
 
 var paths = {
   scripts: [
-    'app.js',
-    'server.js',
-    'api_modules/*.js',
+    'gulpfile.js',
+    'src/app.js',
+    'src/server.js',
     'tests/*.js',
-    'schemas/*.js',
-    'include/*.js',
-    'shared/*.js',
-    'gulpfile.js'
+    'src/**/*.js'
+  ],
+  tests: [
+    'tests/utils.test.js',
+    'tests/controlValidation.test.js',
+    'tests/initApi.test.js',
+    'tests/userApi.test.js',
+    'tests/projectApi.test.js',
+    'tests/deviceApi.test.js',
+    'tests/profileApi.test.js'
+  ],
+  browserifySrc: [
+    'src/client/authModule.js', 
+    'src/client/dashboardModule.js'
+  ],
+  browserifyDest: 'public/js/bundle.js',
+  lessSrc: [ 
+    'public/less/kube.less',
+    'public/less/auth.less',
+    'public/less/dashboard.less'
   ]
 };
+
+gulp.task('less', function () {
+  return gulp.src(lessSrc, { base: './' })
+    .pipe(less())
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('browserify', function() {
+    return gulp.src(paths.browserifySrc, {
+      base: './'
+    })
+        .pipe(browserify({
+          insertGlobals : true,
+          debug : !gulp.env.production
+        }))
+        .pipe(gulp.dest(paths.browserifyDest));
+});
 
 gulp.task('jshint', function () {
   return gulp.src(paths.scripts, {
@@ -40,12 +77,34 @@ gulp.task('prettify', function () {
       config: '.jsbeautifyrc',
       mode: 'VERIFY_AND_WRITE'
     }))
-    .pipe(gulp.dest('./'))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('test', function () {
+  return gulp.src(paths.tests, {
+      read: false
+    })
+    .pipe(mocha({
+      reporter: 'spec',
+      bail: true,
+      colors: true,
+      timeout: 30000
+    }));
+
 });
 
 
 gulp.task('watch', function () {
+  isWatching = true;
   gulp.watch(paths.scripts, ['jshint']);
 });
 
-gulp.task('default', ['jshint', 'prettify']);
+gulp.task('default', ['jshint', 'prettify', 'browserify', ]);
+
+gulp.on('stop', function () {
+  if (!isWatching) {
+    process.nextTick(function () {
+      process.exit(0);
+    });
+  }
+});
