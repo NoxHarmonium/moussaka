a3e25fead5cffad3a24d56877fe9fa0534f6f9a6
Sean Dawson <sean@two-bulls.com>
2014-09-15 21:50:50 -0300

    Added in new test file to the gencoverage script.

--- gencoverage.sh
+++ gencoverage.sh
@@ -8,5 +8,5 @@ else
     echo "Coveralls info is already set. This should be so on Travis CI."
 fi
 
-FILES="tests/utils.test.js tests/controlValidation.test.js tests/userApi.test.js tests/projectApi.test.js tests/deviceApi.test.js tests/profileApi.test.js"
+FILES="tests/utils.test.js tests/controlValidation.test.js tests/initApi.test.js tests/userApi.test.js tests/projectApi.test.js tests/deviceApi.test.js tests/profileApi.test.js"
 istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- -t 30000 -R spec -b $FILES && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rm -rf ./coverage
