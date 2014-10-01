b15632df9fbe86271409ae449d7463c2065a75d6
Sean Dawson <contact@seandawson.info>
2014-10-01 13:10:12 -0300

    Added bower cache clean which can fix issues with bower on heroku.

--- postinstall.sh
+++ postinstall.sh
@@ -1,12 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 NODE_PATH="node"
-PACKAGE_DIR="./node_modules"
+PACKAGE_DIR="node_modules"
 
 GULP="$PACKAGE_DIR/gulp/bin/gulp.js"
 BOWER="$PACKAGE_DIR/bower/bin/bower"
 
 echo "Running bower to download client dependencies..."
+$BOWER cache clean
 $BOWER install
 
 echo "Running gulp to compile css and js bundles..."
