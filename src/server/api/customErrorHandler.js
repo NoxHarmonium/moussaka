e786a2ad61de72a410c3867b50eb755e59035f8f
Sean Dawson <contact@seandawson.info>
2014-10-09 15:15:51 -0300

    Fixed a serious bug that was swollowing uncaught exceptions and causing the server to hang.

--- src/server/api/customErrorHandler.js
+++ src/server/api/customErrorHandler.js
@@ -5,11 +5,15 @@
     require('mongoose/lib/error/validation');
   var InvalidQueryParams =
     require('../include/invalidQueryParams.js');
+  var colors = require('colors');
 
   module.exports = function (err, req, res, next) {
     // Client Errors (400)
 
     if (err instanceof MongooseValidationError) {
+      //console.log('Invalid Client Request:  '.red,
+      //  '[MongooseValidationError]'.yellow,
+       // err.toString());
       return res.status(400)
         .send({
           type: 'ValidationError',
@@ -18,6 +22,9 @@
     }
 
     if (err instanceof InvalidQueryParams) {
+      //console.log('Invalid Client Request:  '.red,
+      //  '[InvalidQueryParams]'.yellow,
+      //  err.toString());
       return res.status(400)
         .send({
           type: 'InvalidQueryParams',
@@ -25,5 +32,8 @@
           params: err.params
         });
     }
+
+    // Pass her right along through
+    next(err);
   };
 })(require, module);
